0b3ca2358f51b8edd279de348fe0cf91b7d05c3d
dsam36828 <159868637+dsam36828@users.noreply.github.com>
2024-02-25 20:26:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,35 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+// Use the body-parser middleware for parsing JSON bodies
+app.use(express.json());
 
 // List of allowed frontend origins for CORS
 const allowedOrigins = [
-  "https://musubijp.com",
-  "http://musubijp.com",
-  "http://127.0.0.1:5501",
-  "http://127.0.0.1:5500",
-  "https://halcyoninjp.live",
-  "http://halcyoninjp.live",
-  "https://halcyoninjp.live/",
-  "http://halcyoninjp.live/",
-  "https://hotnihonnews.today",
-  "https://hotnihonnews.today/",
-  "http://hotnihonnews.today",
-  "http://hotnihonnews.today/",
-  "https://ufitmerchandise.in",
-  "http://ufitmerchandise.in",
-  "https://ufitmerchandise.in/",
-  "http://ufitmerchandise.in/",
-  "https://nihonfuku.shop",
-  "https://nihonfuku.shop/",
-  "http://nihonfuku.shop",
-  "http://nihonfuku.shop/",
-];
-
-// List of allowed referrers
-const allowedReferrers = [
   "https://musubijp.com",
   "http://musubijp.com",
   "http://127.0.0.1:5501",
@@ -66,43 +42,16 @@ const corsOptions = {
   },
 };
 
-// Apply the CORS middleware
+// Apply the CORS middleware with the configured options
 app.use(cors(corsOptions));
 
-// Check against the allowedReferrers
-app.get(
-  "/",
-  (req, res, next) => {
-    const referer = req.headers.referer;
-
-    // Check if the referer exists in the allowedReferrers array
-    if (
-      referer &&
-      allowedReferrers.some((domain) => referer.startsWith(domain))
-    ) {
-      next();
-    } else {
-      res.status(403).send("Access forbidden");
-    }
-  },
-  (req, res) => {
+// Example route
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
+    // Alternatively, you can uncomment the line below to send an iframe or any other response
     // res.send(`<iframe width="100%" height="100%" margin-top:"30%" src="https://www.youtube.com/embed/463tZXEDhig?si=okMgnV6S1RF1XDhN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
-  }
-);
-
-// app.use(cors());
-
-// app.post("/submit-phone", (req, res) => {
-//   // Log the phone number on the server side
-//   console.log("Received phone number:", req.body);
-
-//   res.header("Access-Control-Allow-Origin", "*"); // Allow CORS from any origin
-
-//   // Send a response back to the client
-//   res.json({ message: "Phone number submitted successfully" });
-// });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running on port ${PORT}`);
 });
